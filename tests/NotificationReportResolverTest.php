1306dadb18a945d3d1c24a757b853b91c7db875b
Steven Grant <steven@ghijk.nz>
2026-08-12 17:54:11 +1200

    Complete report behavior coverage

--- tests/NotificationReportResolverTest.php
+++ tests/NotificationReportResolverTest.php
@@ -18,6 +18,47 @@ use Statamic\Entries\Entry;
 
 class NotificationReportResolverTest extends TestCase
 {
+    public function test_acknowledgement_status_is_resolved_live_on_each_report(): void
+    {
+        $user = Mockery::mock(User::class);
+        $user->allows('id')->andReturn('user-1');
+        $user->allows('hasRole')->andReturnFalse();
+        $user->allows('isInGroup')->andReturnFalse();
+        $users = Mockery::mock(UserRepository::class);
+        $users->allows('all')->andReturn(new UserCollection([$user]));
+        $acknowledgement = null;
+        $acknowledgements = Mockery::mock(AcknowledgementRepository::class);
+        $acknowledgements->allows('find')->andReturnUsing(
+            function () use (&$acknowledgement) {
+                return $acknowledgement;
+            },
+        );
+        $acknowledgements->allows('forNotification')->andReturn(collect());
+        $snoozes = Mockery::mock(SnoozeRepository::class);
+        $snoozes->allows('find')->andReturnNull();
+        $resolver = new NotificationReportResolver(
+            new AudienceResolver($users, new AudienceMatcher),
+            $acknowledgements,
+            $snoozes,
+            $users,
+        );
+        $notice = (new Entry)
+            ->id('notice-1')
+            ->collection(Collection::make('notifications'))
+            ->set('audience', ['all' => true]);
+
+        $this->assertNull($resolver->resolve($notice)->sole()['acknowledgement']);
+
+        $acknowledgement = new Acknowledgement(
+            'ack-1',
+            'notice-1',
+            'user-1',
+            CarbonImmutable::parse('2026-08-12 12:00'),
+        );
+
+        $this->assertSame('ack-1', $resolver->resolve($notice)->sole()['acknowledgement']->id);
+    }
+
     public function test_role_removal_changes_targeting_but_preserves_the_acknowledgement(): void
     {
         $hasRole = true;
